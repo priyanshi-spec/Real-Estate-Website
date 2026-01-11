4f30980409cc458e3e70ca3a6f81539866700b85
priyanshi-spec <priyanshihomkar47@gmail.com>
2026-01-11 16:10:07 +0530

    removed bootstrap
    because of no use

--- html/index.php
+++ html/index.php
@@ -67,7 +67,6 @@
 <?php include 'footer1.php';?>
 </footer>
 <script src="../javascript/estate.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
